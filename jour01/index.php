cd05510add7994c6178d592c868cd66e811ba222
hassine-zamouri <91829151+hassine-zamouri@users.noreply.github.com>
2021-11-02 15:32:51 +0100

    commencement des variables, valeurs...

---
+++ jour01/index.php
@@ -0,0 +1,24 @@
+<?php 
+echo "Hello la Plateforme";
+
+$str = ' La Plateforme';
+$str2 = ' Vive';
+$str3 = ' ! ';
+echo "<br>";
+
+echo  $str2 , $str , $str3;
+echo "<br>";
+
+$val = 6;
+echo $val;
+$val += 4;
+echo $val;
+echo "<br>";
+//$val =$val+4;
+
+$mybool = true;
+echo $mybool;
+$mybool = false;
+echo $mybool;
+
+?>
